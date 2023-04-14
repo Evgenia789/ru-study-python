9539e7cdba808559685279844e111026cd75eb41
Evgeniia Pankova <jen.backulina@yandex.ru>
2023-04-14 17:01:48 +0200

    Add a solution to the maps problem

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,21 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+        def check_countries_and_rating(movie: dict) -> Union[int, None]:
+            if "," in movie["country"] and movie["rating_kinopoisk"]:
+                if (ratings := float(movie["rating_kinopoisk"])) > 0:
+                    return ratings
+            return None
+
+        ratings = [
+            rating
+            for rating in map(lambda x: check_countries_and_rating(x), list_of_movies)
+            if rating is not None
+        ]
+        sum_ratings = sum(ratings)
+        avg_ratings = sum_ratings / len(ratings)
+
+        return avg_ratings
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +42,18 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+        def count_letter(title: str) -> int:
+            count = 0
+            for letter in title:
+                if letter == "и":
+                    count += 1
+            return count
+
+        def check_rating(movie: dict) -> bool:
+            if movie["rating_kinopoisk"] and float(movie["rating_kinopoisk"]) >= rating:
+                return count_letter(movie["name"])
+            return 0
+
+        result = sum([count for count in map(lambda x: check_rating(x), list_of_movies)])
+
+        return result
